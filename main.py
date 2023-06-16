fc54d060a747ef85e812a27a736e739a9cd56353
Mohamed Eltayeb <moheltbusiness@gmail.com>
2023-06-16 19:51:06 +0100

    Commit: Add menu

--- main.py
+++ main.py
@@ -11,8 +11,28 @@ import os
 class PDFViewer:
     # initializing the __init__ / special method
     def __init__(self, master):
-        pass
-    
+        # creating the window
+        self.master = master
+        # gives title to the main window
+        self.master.title('PDF Viewer')
+        # gives dimensions to main window
+        self.master.geometry('720x900')
+        # this disables the minimize/maximize button on the main window
+        self.master.resizable(width = 0, height = 0)
+        # loads the icon and adds it to the main window
+        self.master.iconbitmap(self.master, 'pdf_file_icon.png')
+        
+        # creating the menu
+        self.menu = Menu(self.master)
+        # adding it to the main window
+        self.master.config(menu=self.menu)
+        # creating a sub menu
+        self.filemenu = Menu(self.menu)
+        # giving the sub menu a label
+        self.menu.add_cascade(label="File", menu=self.filemenu)
+        # adding a two buttons to the sub menus
+        self.filemenu.add_command(label="Open File")
+        self.filemenu.add_command(label="Exit")
     
 # creating the root window using Tk() class
 root = Tk()
