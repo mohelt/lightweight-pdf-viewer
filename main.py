ba79aedfd874f93a2a7fd649231f2985fc7386c4
Mohamed Eltayeb <moheltbusiness@gmail.com>
2023-06-16 20:08:09 +0100

    Commit: miner.py

--- main.py
+++ main.py
@@ -32,8 +32,6 @@ class PDFViewer:
         self.menu.add_cascade(label="File", menu=self.filemenu)
         # adding a two buttons to the sub menus
         self.filemenu.add_command(label="Open File")
-        self.filemenu.add_command(label="Exit")
-        
                 # creating the top frame
         self.top_frame = ttk.Frame(self.master, width=700, height=900)
         # placing the frame using inside main window using grid()
@@ -74,6 +72,23 @@ class PDFViewer:
         self.scrollx = Scrollbar(self.top_frame, orient=HORIZONTAL)
         # adding the scrollbar
         self.scrollx.grid(row=1, column=0, sticky=(W, E))
+
+        # creating the canvas for display the PDF pages
+        self.output = Canvas(self.top_frame, bg='#ECE8F3', width=670, height=870)
+        # inserting both vertical and horizontal scrollbars to the canvas
+        self.output.configure(yscrollcommand=self.scrolly.set, xscrollcommand=self.scrollx.set)
+        # adding the canvas
+        self.output.grid(row=0, column=0)
+        # configuring the horizontal scrollbar to the canvas
+        self.scrolly.configure(command=self.output.yview)
+        # configuring the vertical scrollbar to the canvas
+        self.scrollx.configure(command=self.output.xview)
+
+        # configuring the vertical scrollbar to the canvas
+        self.scrollx.configure(command=self.output.xview)
+        self.filemenu.add_command(label="Exit", command=self.master.destroy)
+        
+        
 # creating the root window using Tk() class
 root = Tk()
 # instantiating/creating object app for class PDFViewer
